8d6b4a892ad0814b3f4c0f6365da3825a8024231
sebthom <sebthom@users.noreply.github.com>
2022-02-24 11:36:55 +0100

    update copyright info

--- plugin/src/main/java/de/sebthom/eclipse/open_with_eclipse/localization/Messages.java
+++ plugin/src/main/java/de/sebthom/eclipse/open_with_eclipse/localization/Messages.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 by Sebastian Thomschke and contributors
+ * Copyright 2021-2022 by Sebastian Thomschke and contributors
  * SPDX-License-Identifier: EPL-2.0
  */
 package de.sebthom.eclipse.open_with_eclipse.localization;
